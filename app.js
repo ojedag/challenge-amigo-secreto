28cbd49f73ba2ec1f326f036acae450945647380
Gregorio Ojeda <ojeda.t.2000@gmail.com>
2025-08-08 09:44:22 -0500

    implementa funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function limpiarCampo() {
+    document.getElementById('amigo').value = '';
+}
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('amigo').value;
+    if (inputAmigo === "") {
+        alert("¡Por favor, inserte un nombre!");
+    } else if (amigos.includes(inputAmigo)) {
+        alert(`${inputAmigo} ya exite en la lista, ingresar otro por favor`);
+        limpiarCampo();
+    } else {
+        amigos.push(inputAmigo);
+        limpiarCampo();
+    }
+    return amigos;
+}
